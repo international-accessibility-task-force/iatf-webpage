becfa7fb935cb1de9ff0e251e8223721b77b320b
mugiwarafx <58401248+mugiwarafx@users.noreply.github.com>
2024-02-17 21:38:25 +0100

    feat: vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,8 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 injectSpeedInsights();
+inject({ mode: dev ? 'development' : 'production' });
 
 export const prerender = true;
